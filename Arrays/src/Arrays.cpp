f06b2bdbf793ba36d073612163a6d344548457ec
Kevin Hallman <musicturtul@192.168.1.103>
2018-06-02 03:41:45 -0400

    found out about initializing the array

--- Arrays/src/Arrays.cpp
+++ Arrays/src/Arrays.cpp
@@ -16,8 +16,28 @@ int main() {
 	values[1] = 123;
 	values[2] = 7;
 
+	cout << "Array of integers" << endl;
+	cout << "-----------------" << endl;
 	cout << values[0] << endl;
 	cout << values[1] << endl;
 	cout << values[2] << endl;
+
+	double numbers[4] = {3.4, 2.1, 5.2, 7.6};
+
+	cout << endl << "Array of doubles" << endl;
+	cout << "-----------------" << endl;
+	cout << numbers[2] << endl;
+
+	 for (int i = 0; i < 4; i++) {
+		 cout << numbers[i] << endl;
+	 }
+// if you don't put anything in the brackets it will initialize at 0
+	 cout << endl << "Initializing with zero value" << endl;
+	 cout << "----------------------------" << endl;
+
+	 int numberArray[8] = {};
+	 for (int i = 0; i < 8; i++) {
+		 cout << "Element at index " << i << ": " << numberArray[i] << endl;
+	 }
 	return 0;
 }
